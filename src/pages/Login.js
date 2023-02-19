5cacdba0ff11976eb68be87ea02ab34c78a792c3
ByungJun Kang <44726494+BangDori@users.noreply.github.com>
2023-02-19 22:41:20 +0900

    ADD: 로그인 디자인 및 기능 추가

---
+++ src/pages/Login.js
@@ -0,0 +1,89 @@
+import styled from "styled-components";
+import React, { useState } from "react";
+
+const LoginBox = styled.form`
+  position: relative;
+  text-align: center;
+
+  & .title {
+    margin: 8px 0 24px;
+    font-size: 32px;
+    font-weight: bold;
+    color: #ffc0cb;
+  }
+
+  & input {
+    display: block;
+
+    width: 320px;
+    height: 44px;
+    border-radius: 10px;
+
+    padding-inline-start: 24px;
+    background: #ecf2ff;
+
+    opacity: 0.7;
+    border: none;
+  }
+
+  & input:focus {
+    outline: 3px solid #ea8fea;
+  }
+
+  & input + input {
+    margin: 8px 0 32px;
+  }
+
+  & button {
+    width: 144px;
+    height: 48px;
+    font-size: 12px;
+    border: none;
+    border-radius: 10px;
+
+    background: #85cdfd;
+    font-weight: bold;
+
+    margin: 0 4px;
+    cursor: pointer;
+  }
+
+  & .find {
+    color: #ccc;
+    position: absolute;
+    right: 16px;
+    bottom: -48px;
+
+    cursor: pointer;
+  }
+
+  & .find > span + span {
+    margin-left: 16px;
+  }
+
+  & .find > span:hover {
+    color: blue;
+  }
+`;
+
+const Login = () => {
+  const [userID, setUserID] = useState("");
+  const [userPWD, setUserPWD] = useState("");
+
+  return (
+    <LoginBox action="" method="post">
+      <div className="title">Login</div>
+      {/* 화면 진입 시 Input ID를 가리키도록 활성화 필요 */}
+      <input name="userID" placeholder="Input ID" />
+      <input name="userPWD" placeholder="Input Password" />
+      <button type="submit">Login</button>
+
+      <div className="find">
+        <span>아이디 찾기</span>
+        <span>비밀번호 찾기</span>
+      </div>
+    </LoginBox>
+  );
+};
+
+export default Login;
