d57786ff0e1e0b81abefd0f7331c423aa915559c
ByungJun Kang <44726494+BangDori@users.noreply.github.com>
2023-02-19 22:41:55 +0900

    Remove: 자식 컴포넌트로 이동

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import styled from "styled-components";
-import { Route, Routes, Link } from "react-router-dom";
-import React from "react";
-import Home from "./Home";
-import Register from "./Register";
-import Login from "./Login";
-import Todos from "./Todos";
+import Menu from "./components/Menu";
+import ContentList from "./pages/ContentList";
 
-const fns = [
+const menus = [
   {
     id: 1,
     title: "Home",
@@ -49,58 +45,11 @@ const Box = styled.div`
   }
 `;
 
-const FnBox = styled.div`
-  display: inline-block;
-  text-align: center;
-  width: 50%;
-
-  & > div {
-    width: 320px;
-    margin: 0 auto;
-    padding: 32px 0;
-    text-align: center;
-
-    border: 1px solid #b9f3e4;
-  }
-
-  & a {
-    font-size: 18px;
-  }
-
-  & a:hover {
-    color: #a459d1;
-  }
-`;
-
-const Contents = styled.div`
-  display: inline-block;
-
-  width: 50%;
-`;
-
 const App = () => {
   return (
     <Box>
-      <FnBox>
-        <div>
-          <ul>
-            {fns.map((fn) => (
-              <li key={fn.id}>
-                <Link to={fn.link}>{fn.title}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </FnBox>
-
-      <Contents>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/todos" element={<Todos />} />
-        </Routes>
-      </Contents>
+      <Menu menus={menus} />
+      <ContentList />
     </Box>
   );
 };
