7ecbb820de2c3aeb617403b121e508322096a563
BangDori <qudwns8865@naver.com>
2023-02-20 16:00:22 +0900

    Add: Login & Logout 구분

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,26 +3,48 @@ import cn from "classnames";
 import React from "react";
 import MenuBox from "../styles/components/MenuBox";
 
-const Menu = ({ menus }) => {
+const Menu = ({ isLogin }) => {
   const location = useLocation();
   const { pathname } = location;
 
   return (
     <MenuBox>
       <div className="menu_list">
+        {isLogin.status ? <h3>{isLogin.name}님 환영합니다.</h3> : null}
         <ul>
-          {menus.map((menu) => {
-            return (
-              <li key={menu.id}>
-                <Link
-                  to={menu.link}
-                  className={cn({ selected: menu.link === pathname })}
-                >
-                  {menu.title}
-                </Link>
-              </li>
-            );
-          })}
+          <li>
+            <Link to="/" className={cn({ selected: pathname === "/" })}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/register"
+              className={cn({ selected: pathname === "/register" })}
+            >
+              Register
+            </Link>
+          </li>
+          <li>
+            {isLogin.status ? (
+              <Link to="/logout">Logout</Link>
+            ) : (
+              <Link
+                to="/login"
+                className={cn({ selected: pathname === "/login" })}
+              >
+                Login
+              </Link>
+            )}
+          </li>
+          <li>
+            <Link
+              to="/todos"
+              className={cn({ selected: pathname === "/todos" })}
+            >
+              Todos
+            </Link>
+          </li>
         </ul>
       </div>
     </MenuBox>
--- src/pages/ContentList.js
+++ src/pages/ContentList.js
@@ -1,19 +1,31 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Home from "./Home";
-import Login from "./Login";
-import Register from "./Register";
+import RegisterContainer from "../containers/RegisterContainer";
+import LoginContainer from "../containers/LoginContainer";
 import Todos from "./Todos";
 import ListBox from "../styles/pages/ListBox";
+import Logout from "./Logout";
 
-const ContentList = () => {
+const ContentList = ({ isLogin, onLogin }) => {
+  const navigate = useNavigate();
+
+  const onClick = (form) => {
+    onLogin(form);
+    navigate("/");
+  };
+
   return (
     <ListBox>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/todos" element={<Todos />} />
+        <Route path="/register" element={<RegisterContainer />} />
+        {isLogin.status ? (
+          <Route path="/logout" element={<Logout onLogout={onClick} />} />
+        ) : (
+          <Route path="/login" element={<LoginContainer onLogin={onClick} />} />
+        )}
+        <Route path="/todos" element={<Todos isLogin={isLogin} />} />
       </Routes>
     </ListBox>
   );
