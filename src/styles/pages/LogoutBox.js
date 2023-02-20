fe22573a14868b81d1fd3ff2ed7a85cae056b0ca
BangDori <qudwns8865@naver.com>
2023-02-20 15:58:53 +0900

    Add: Logout 스타일

---
+++ src/styles/pages/LogoutBox.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const LogoutBox = styled.div`
+  display: inline-block;
+  width: 360px;
+  text-align: center;
+
+  & button {
+    width: 144px;
+    height: 48px;
+    font-size: 12px;
+    font-weight: bold;
+    background: #85cdfd;
+    border: none;
+    border-radius: 10px;
+
+    cursor: pointer;
+  }
+`;
+
+export default LogoutBox;
