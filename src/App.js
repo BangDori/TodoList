9397311c41c609314e2a78cede36a43668b21c47
BangDori <qudwns8865@naver.com>
2023-02-20 15:18:38 +0900

    Remove: menus 제거

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
+import { useState } from "react";
 import Menu from "./components/Menu";
 import ContentList from "./pages/ContentList";
 import Box from "./styles/Box";
 
-const menus = [
-  {
-    id: 1,
-    title: "Home",
-    link: "/",
-  },
-  {
-    id: 2,
-    title: "Register",
-    link: "/register",
-  },
-  {
-    id: 3,
-    title: "Login",
-    link: "/login",
-  },
-  {
-    id: 4,
-    title: "Todos",
-    link: "/todos",
-  },
-];
-
 const App = () => {
+  const [isLogin, setIsLogin] = useState({
+    name: "",
+    status: false,
+  });
+
   return (
     <Box>
-      <Menu menus={menus} />
-      <ContentList />
+      <Menu isLogin={isLogin} onLogout={setIsLogin} />
+      <ContentList isLogin={isLogin} onLogin={setIsLogin} />
     </Box>
   );
 };
