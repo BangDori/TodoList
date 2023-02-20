233579f7cf518dda708cec431de265c463710545
BangDori <qudwns8865@naver.com>
2023-02-20 14:55:55 +0900

    Add: 정규표현식

---
+++ src/utils/regExp.js
@@ -0,0 +1,10 @@
+/**
+ * id, password, name 체크를 위한 정규표현식
+ */
+const regExpID = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d].{3,}$/;
+const regExpPW =
+  /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]*$/;
+const regExpName = /^[A-Za-z\d[ㄱ-ㅎㅏ-ㅣ가-힣].{1,}$/;
+const patternSpec = /[~!@#$%^&*()_+|<>?:{}]/; // 특수문자
+
+export { regExpID, regExpPW, regExpName, patternSpec };
