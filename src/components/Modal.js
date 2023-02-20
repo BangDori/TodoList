53e2ab6d1a56050b1e00cc82e4048a6b04bacd63
ByungJun Kang <44726494+BangDori@users.noreply.github.com>
2023-02-20 23:19:33 +0900

    Add: 아이디, 비밀번호 찾기

---
+++ src/components/Modal.js
@@ -0,0 +1,64 @@
+import { useState, useRef } from "react";
+import ModalBox from "../styles/pages/Modal";
+import "rodal/lib/rodal.css";
+import Rodal from "rodal";
+import { findUser } from "../utils/user";
+
+const Modal = ({ subject, visible, onHide }) => {
+  const [name, setName] = useState("");
+  const [id, setId] = useState("");
+
+  const Message = useRef(null);
+  const onClick = async () => {
+    try {
+      if (subject === "id") {
+        const searchID = await findUser(name, "name");
+        Message.current.textContent = `ID: ${searchID[0].user_id}`;
+      } else {
+        const searchPWD = await findUser(id, "id");
+        Message.current.textContent = `Password: ${searchPWD[0].user_password}`;
+      }
+    } catch (e) {
+      Message.current.textContent = e.message;
+      setTimeout(() => {
+        Message.current.textContent = "";
+      }, 3000);
+    }
+
+    setName("");
+    setId("");
+  };
+
+  return (
+    <ModalBox>
+      <Rodal visible={visible} onClose={onHide}>
+        <div className="modal_title">
+          {subject === "id" ? "Find ID" : "Find Password"}
+        </div>
+
+        {subject === "id" ? (
+          <input
+            name="name"
+            placeholder="Input Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        ) : (
+          <input
+            name="id"
+            placeholder="Input ID"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
+        )}
+
+        <button className="login_button search" onClick={onClick}>
+          Click
+        </button>
+        <div ref={Message} className="message_box"></div>
+      </Rodal>
+    </ModalBox>
+  );
+};
+
+export default Modal;
