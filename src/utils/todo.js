2b0f3a9fa665a6df7952f05ba0df3754818d6115
BangDori <qudwns8865@naver.com>
2023-02-23 16:22:27 +0900

    Add: TodoList CRUD

---
+++ src/utils/todo.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+
+export async function getTodoList(name) {
+  try {
+    const todoList = await axios({
+      url: `http://localhost:4000/${name}`,
+      method: "get",
+      responseType: "json",
+    }).then((result) => {
+      return result.data;
+    });
+
+    return todoList;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export async function insertTodoList(name, nextTodos) {
+  try {
+    await axios({
+      url: `http://localhost:4000/${name}`,
+      method: "post",
+      data: nextTodos,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export async function updateTodoList(name, id, todo) {
+  try {
+    await axios({
+      url: `http://localhost:4000/${name}/${id}`,
+      method: "patch",
+      data: { ...todo, checked: !todo.checked },
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export async function removeTodoList(name, id) {
+  try {
+    await axios({
+      url: `http://localhost:4000/${name}/${id}`,
+      method: "delete",
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
