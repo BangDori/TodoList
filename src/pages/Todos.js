cb4fae51eed3541e270a51a2c9e8dfc131f7b9ae
BangDori <qudwns8865@naver.com>
2023-02-23 16:20:53 +0900

    Add: Todos 페이지

--- src/pages/Todos.js
+++ src/pages/Todos.js
@@ -1,21 +1,16 @@
-import TodosBox from "../styles/pages/TodosBox";
+import React from "react";
+import TodoTemplate from "./todo/TodoTemplate";
 
 const Todos = ({ isLogin }) => {
   if (!isLogin.status) {
     return (
-      <TodosBox>
-        <h3 className="default-todos">로그인 후 이용가능한 시스템입니다.</h3>
-      </TodosBox>
-    );
-  } else {
-    return (
-      <TodosBox>
-        <p className="title">
-          <strong>Todo List</strong>
-        </p>
-      </TodosBox>
+      <>
+        <h3 style={{ color: "red" }}>로그인 후 이용가능한 시스템입니다.</h3>
+      </>
     );
   }
+
+  return <TodoTemplate isLogin={isLogin} />;
 };
 
 export default Todos;
