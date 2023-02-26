06b6c507a9979a50a17ffcfe3c1d172a048365a3
BangDori <qudwns8865@naver.com>
2023-02-26 18:10:58 +0900

    Feat: 최적화를 위한 가상화 테이블 기능

--- src/pages/todo/TodoList.js
+++ src/pages/todo/TodoList.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { List } from 'react-virtualized';
 import TodoListItem from './TodoListItem';
 
 const TodoList = ({ todos, onToggle, onRemove }) => {
-  return (
-    <>
-      {todos.map((todo) => (
+  /**
+   * 기본적으로, react-virtualized는 오직 화면에 보여지는 행들만 렌더링한다.
+   * 하지만, 유저가 빠르게 스클롤링 할 때와 같은 경우에
+   * 유저 스크롤링을 유지하기 위해 추가적인 행을 렌더링할 필요가 있다.
+   * 부드러운 스크롤을 보장하기 위해, react-virtaulized 라이브러리는 'overscanRowCount'(기본값 10)를 제공한다.
+   * 대부분의 경우 최적의 성능을 위해 최소 overscanRowCount를 10으로 정하는것을 권장한다.
+   */
+  const rowRenderer = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
         <TodoListItem
-          key={todo.id}
           todo={todo}
-          onToggle={onToggle}
+          key={key}
           onRemove={onRemove}
+          onToggle={onToggle}
+          style={style}
         />
-      ))}
-    </>
+      );
+    },
+    [onRemove, onToggle, todos],
+  );
+
+  return (
+    <List
+      className='TodoList'
+      width={360} // View 전체 크기
+      height={336} // View 전체 높이
+      rowCount={todos.length} // 항목의 전체 개수
+      rowHeight={42} // 항목 높이
+      rowRenderer={rowRenderer} // 렌더링 할 항목
+      list={todos} // 배열
+      style={{ outline: 'none' }} // list에 기본으로 적용되는 outline 스타일 제거
+    />
   );
 };
 
-export default TodoList;
+export default React.memo(TodoList);
