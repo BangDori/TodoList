132470cf44be03724354d963d0f84b38eb653d2a
BangDori <qudwns8865@naver.com>
2023-03-02 15:40:51 +0900

    Feat: redux, redux tool 추가

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import rootReducer from './store';
+import { legacy_createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const store = legacy_createStore(rootReducer, composeWithDevTools());
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
