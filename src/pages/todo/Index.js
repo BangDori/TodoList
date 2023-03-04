19238f66251bdb6961a3f244912d1ef955244089
BangDori <qudwns8865@naver.com>
2023-03-04 10:55:33 +0900

    Feat: 투두템플릿 분리

--- src/pages/todo/Index.js
+++ src/pages/todo/Index.js
@@ -1,20 +1,9 @@
 // Todo index page
 import React from 'react';
-import { useSelector } from 'react-redux';
-import TodoTemplate from './TodoTemplate';
+import loadable from '@loadable/component';
+const TodoTemplate = loadable(() => import('./TodoTemplate'));
 
 const Index = () => {
-  const { status } = useSelector((state) => state.user);
-
-  if (!status) {
-    // 로그인 되지 않았을 경우
-    return (
-      <>
-        <h3 style={{ color: 'red' }}>로그인 후 이용가능한 시스템입니다.</h3>
-      </>
-    );
-  }
-
   // 로그인 되었을 경우
   return <TodoTemplate />;
 };
