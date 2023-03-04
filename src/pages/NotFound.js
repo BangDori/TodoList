c7ee5b19ba8779293b31d8fb60e3ba7b7f7babf7
BangDori <qudwns8865@naver.com>
2023-03-04 10:53:24 +0900

    Feat: 에러 페이지 분리

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,8 +1,18 @@
-// Not Found page
-import React from 'react';
+import { useEffect, useState } from 'react';
 
-const NotFound = () => {
-  return <h2 style={{ color: 'red' }}>404 Not Found Page</h2>;
+// Not Found page
+const NotFound = ({ type }) => {
+  const [message, setMessage] = useState('존재하지 않는 페이지 입니다.');
+
+  useEffect(() => {
+    if (type === 'login') {
+      setMessage('로그인 후 이용가능한 시스템입니다.');
+    }
+
+    return () => setMessage('존재하지 않는 페이지 입니다.');
+  }, [type]);
+
+  return <h3 style={{ color: 'red' }}>{message}</h3>;
 };
 
 export default NotFound;
